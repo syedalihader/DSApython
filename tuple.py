3b18f84e426210efe95d8be423a5427c3cf38542
syedalihaider <syedalihaider03@gmail.com>
2023-03-30 12:16:35 +0500

    lab2

---
+++ tuple.py
@@ -0,0 +1,77 @@
+
+'''
+#touple
+empty_tuple=()
+empty_tuple = tuple
+
+#Tuple with initial values
+
+tpl = ('item1', 'item2','item3')
+fruits = ('banana', 'orange', 'mango', 'lemon')
+
+
+#Tuple length
+
+tpl = ('item1', 'item2', 'item3')
+len(tpl)
+
+tuple =( 'item1 ' ,'item2', 'item3', 'item4')
+first_item = tuple[0]
+secound_item =tuple[1]
+
+
+fruits = ('banana','orange','mango','lemon')
+first_fruit = fruits [0]
+
+secound_fruits= fruits [1]
+lant_index=len(fruits) -1
+last_index= fruits 
+
+#syntex
+tuple =( 'item1 ' ,'item2', 'item3', 'item4')
+first_item = tuple[-4]
+secound_item =tuple[-3]
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
+
+print (secound_item)
+print(lant_index)
+print(last_fruit)
+print( lant_index)
+print(first_item)
+print(fruits)
+print(tpl)
+print(first_fruit)
+print(last_fruit)
+'''
+#positive index
+'''#lsicing tuple 
+tuple = ('item1','item2','item3','item4')
+all_items= tuple[0:4]
+all_items=tuple[0:]
+middle_two_items=tuple[1:3]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]    
+all_fruits= fruits[0:]      
+orange_mango = fruits[1:3]  
+orange_to_the_rest = fruits[1:]
+
+#negative index 
+ 
+tpl = ('item1', 'item2', 'item3','item4')
+all_items = tpl[-4:]        
+middle_two_items = tpl[-3:-1] 
+ 
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:] 
+orange_mango = fruits[-3:-1] 
+orange_to_the_rest = fruits[-3:]
+'''
+
+
+
